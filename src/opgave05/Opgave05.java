f0c4d26076559dbeca2dce41c86ce8d5e4fa5407
nakor <nakor22@student.sdu.dk>
2024-02-22 11:49:52 +0100

    Completed task 5

--- src/opgave05/Opgave05.java
+++ src/opgave05/Opgave05.java
@@ -11,8 +11,16 @@ public class Opgave05 {
                 { 1, 2, 1, 2, 1, 2, 0, 2, 0, 0, 4, 0 },
                 { 5, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0 }
         };
-    Absence absence = new Absence(a);
 
+        Absence absence = new Absence(a);
+
+        //absence.print();
+        System.out.println(absence.totalAbsence(1));
+        System.out.println(absence.average(1));
+        System.out.println(absence.StudentsWithoutAbsence());
+        System.out.println(absence.mostAbsence());
+        absence.reset(1);
+        absence.print();
     }
 
 }
--- src/opgave05/models/Absence.java
+++ src/opgave05/models/Absence.java
@@ -8,26 +8,54 @@ public class Absence {
     }
 
     public void print() {
-
+        for (int i = 0; i < absenceSchema.length; i++) {
+            System.out.print("Elev " + (i + 1) + " ");
+            for (int j = 0; j < absenceSchema[i].length; j++) {
+                System.out.print("måned " + (j + 1) + ", fraværsdage " + absenceSchema[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
     public int totalAbsence(int studentNumber) {
-        return 0;
+        int totalAbsenceDays = 0;
+        for (int i = 0; i < absenceSchema[studentNumber - 1].length; i++) {
+            totalAbsenceDays += absenceSchema[studentNumber - 1][i];
+        }
+
+        return totalAbsenceDays;
     }
 
     public double average(int studentNumber) {
-        return 0.0;
+        return (double) totalAbsence(studentNumber) / 12;
     }
 
     public int StudentsWithoutAbsence() {
-        return 0;
+        int noAbsenceStudents = 0;
+        for (int i = 1; i < absenceSchema.length; i++) {
+            if(totalAbsence(i) == 0) {
+                noAbsenceStudents += 1;
+            }
+        }
+        return noAbsenceStudents;
     }
 
     public int mostAbsence() {
-        return 0;
+        int highestAbsence = totalAbsence(1);
+        int studentWithMostAbsence = 0;
+        for (int i = 2; i < absenceSchema.length; i++) {
+            if(totalAbsence(i) > highestAbsence) {
+                highestAbsence = totalAbsence(i);
+                studentWithMostAbsence = i;
+            }
+        }
+
+        return studentWithMostAbsence;
     }
 
     public void reset(int studentNumber) {
-
+        for (int i = 0; i < absenceSchema[studentNumber-1].length; i++) {
+            absenceSchema[studentNumber-1][i] = 0;
+        }
     }
 }
